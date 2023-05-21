dca441ac301e5e8d46b398db0e762375cab2eecb
onenora <altriabot@gmail.com>
2023-05-21 19:00:37 +0800

    del gs

--- pgmpyro_plugins/google_search.py
+++ pgmpyro_plugins/google_search.py
@@ -1,7 +1,7 @@
-'''谷歌搜索Version1.0'''
-import requests
-from urllib import parse
-
+'''谷歌搜索Version2.0'''
+import asyncio
+import urllib.parse
+import random
 from pagermaid import Config, log
 from pagermaid.listener import listener
 from pagermaid.enums import Message
@@ -10,66 +10,35 @@ from pagermaid.utils import lang, pip_install
 pip_install("googlesearch-python")
 from googlesearch import search
 
-@listener(command="google",
-          description=lang('google_des'),
-          parameters="[text]")
-async def google(message: Message):
-    title = None
-    result = {}
-    args = message.text.strip().split()
-    arg = args[1:] if len(args) > 1 else None
-    reply = message.reply_to_message_id if message.reply_to_message_id else None
-    if arg:
-        text = ' '.join(arg)
-    elif reply:
-        text = message.reply_to_message.text if message.reply_to_message.text else message.reply_to_message.caption
-    else:
-        await message.edit(lang("请加入搜索内容~"))
+@listener(command="gs", 
+        description="Search using Google", 
+        parameters="[text/reply]")
+async def gs(message: Message):
+    try:
+        text = message.arguments or message.reply_to_message.text
+        if not text:
+            info = await message.edit(lang(f"请提供搜索词~"))
+            await asyncio.sleep(3)
+            await message.safe_delete()
+            await info.safe_delete()
+            return
 
-    if text:
-        await message.edit(lang(f"正在努力搜索了主人...\n\n{text}"))
-        for i in search(text, advanced=True):
-            result[i.title] = i.url
-            if len(result) > 4:
-                break
+        info = await message.edit(lang(f"🤖正在努力搜索中...\n\n{text}"))
+        url = f"https://www.google.com/search?q={urllib.parse.quote(text)}"
+        result = {}
+        for lang_code in ["en", "zh-cn"]:
+            for item in search(text, num_results=5, lang=lang_code, advanced=True):
+                result[item.url] = item.title
+            await asyncio.sleep(random.random())
         if result:
-            links = '\n'.join(f"{i+1}、 [{item[0]}]({item[1]})" for i, item in enumerate(result.items()))
-            content = f"🔎 | **Google** | [{text}](https://www.google.com/search?q={parse.quote(text)})\n\n{links}"
-            await message.edit(text=content, disable_web_page_preview=True)
+            links = '\n'.join(f"{i+1}. [{title}]({url})" for i, (url, title) in enumerate(result.items()))
+            content = f"🔎 | **Google** | [{text}]({url})\n\n{links}"
+            await info.edit(content, disable_web_page_preview=True)
         else:
-            await message.edit(lang(f"NB,搜索失败了~\n建议手动搜索:[{text}](https://www.google.com/search?q={parse.quote(text)})"))
-
-
-
-
-import telegram
-from urllib import parse
-from googlesearch import search
-from gettext import gettext as _
-
-@listener(command="google",
-          description=_("Search using Google"),
-          parameters="[text]")
-async def google(update: telegram.Update, context: telegram.ext.CallbackContext):
-    title = None
-    result = {}
-    text = ' '.join(context.args)
-    if not text and update.message.reply_to_message:
-        text = update.message.reply_to_message.text or update.message.reply_to_message.caption
-    if text:
-        await update.message.edit_text(_("Searching for {} ...").format(text))
-        for i in search(text):
-            result[i.title] = i.url
-            if len(result) > 4:
-                break
-        if result:
-            links = 'nn'.join(f"{i+1}、 [{item[0]}]({item[1]})" for i, item in enumerate(result.items()))
-            content = f"🔎 | Google | [{text}](https://www.google.com/search?q={parse.quote(text)})nn{links}"
-            await update.message.edit_text(text=content, disable_web_page_preview=True)
-        else:
-            await update.message.edit_text(_("Sorry, I couldn't find any results for {}.").format(text))
-    else:
-        await update.message.edit_text(_("Please provide a search text."))
-
-
-
+            await info.edit(f"卧槽～搜索失败了～\n[建议手动搜索 {text}]({url})")
+    except Exception as e:
+        emsg = f"搜索失败：{e}"
+        info = await message.edit(emsg)
+        await asyncio.sleep(3)
+        await message.safe_delete()
+        await info.safe_delete()
